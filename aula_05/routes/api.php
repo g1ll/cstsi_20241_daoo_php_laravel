a6357d08aeebf22e478bc2e794126766f3c54a97
Gill D3567 <gillvelleda@gmail.com>
2024-08-23 16:16:03 -0300

    feat: criacao da rota de login e rotas protegidas pelo middleware sanctum.

--- aula_05/routes/api.php
+++ aula_05/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\FornecedorController;
+use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\ProdutoController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +17,16 @@ Route::apiResource('fornecedores',FornecedorController::class)
     ->parameters([
         'fornecedores'=>'fornecedor'
     ]);
+
+
+Route::middleware('auth:sanctum')->group(function(){
+
+    Route::apiResource('users',UserController::class);
+    Route::controller(FornecedorController::class)->group(function(){
+            Route::post('fornecedores','store');
+            Route::put('fornecedores/{fornecedor}','update');
+            Route::delete('fornecedores/{fornecedor}','destroy');
+    });
+});
+
+Route::post('login',[LoginController::class,'login']);
