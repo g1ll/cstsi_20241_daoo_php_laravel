054793aa15152df274e031a99391caf9a54d7e77
Gill D3567 <gillvelleda@gmail.com>
2024-08-23 14:02:49 -0300

    feat: criacao das rotas para o recurso fornecedores. Correcao do parametro fornecedore para o singular fornecedor com o metodo parameter(). php artisan route:list --path=api/fornecedor

--- aula_05/routes/api.php
+++ aula_05/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\FornecedorController;
 use App\Http\Controllers\Api\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('produtos',ProdutoController::class);
+
+Route::apiResource('fornecedores',FornecedorController::class)
+    ->parameters([
+        'fornecedores'=>'fornecedor'
+    ]);
